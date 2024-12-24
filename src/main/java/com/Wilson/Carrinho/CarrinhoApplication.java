233d775b9d1a344e1f478d1288e7215a9bb2fdc6
WilsonQdop <wilson.qdop@gmail.com>
2024-12-24 11:43:46 -0300

    Aplicações iniciais

---
+++ src/main/java/com/Wilson/Carrinho/CarrinhoApplication.java
@@ -0,0 +1,13 @@
+package com.Wilson.Carrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Wilson/Carrinho/CarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wilson.Carrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
