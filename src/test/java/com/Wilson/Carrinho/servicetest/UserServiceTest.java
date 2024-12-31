3b578eadfd8ec4161602068ec532ebabda3beea9
WilsonQdop <wilson.qdop@gmail.com>
2024-12-31 18:43:56 -0300

    Adicionando alguns testes de implementação e unitários no service de usuários.

---
+++ src/test/java/com/Wilson/Carrinho/servicetest/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.Wilson.Carrinho.servicetest;
+
+
+import com.Wilson.Carrinho.entity.User;
+import com.Wilson.Carrinho.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    void findById() {
+        User user = new User(1L, "Wilson", "123", "wilson.alt@f4.com");
+        User user1 = new User(2L, "Vilson", "123", "Vilson.alt@f4.com");
+
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user1);
+
+        List<User> resp = this.userService.findAll();
+        assertEquals(users.size(), resp.size());
+
+    }
+
+}
